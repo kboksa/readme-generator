59bb3d39bca426c66bca65c4324f003180175389
kalina <kalina.boksa@gmail.com>
2022-09-18 19:39:32 -0500

    finished questions

--- index.js
+++ index.js
@@ -4,7 +4,86 @@ const fs = require("fs");
 
 const generatePage = require("./utils/generateMarkdown.js");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    name: "welcome",
+    message:
+      "Thenks for using my ReadMe.md generator! You will be presented with options for you REadMe's sections and their respective contents. Hit enter to begin!",
+  },
+  {
+    type: "input",
+    name: "title",
+    message: "What is the project title?",
+    validate: (titleInput) => {
+      if (titleInput) {
+        return true;
+      } else {
+        console.log("Pleae enter your project title!");
+        return false;
+      }
+    },
+  },
+  {
+    type: "input",
+    name: "description",
+    message: " Write a brief description of your project:",
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "Enter usage of the projects:",
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "Chose the appropriate license for this project",
+    choices: [
+      "Apache 2.0",
+      "BSD 3-Clause",
+      "GNU GPLv3.0",
+      "MIT",
+      "Mozilla Public 2.0",
+      "No license",
+    ],
+  },
+  {
+    type: "input",
+    name: "contribution",
+    message: "Enter you project contribution guidelines:",
+  },
+  {
+    type: "input",
+    name: "test",
+    message: "Please provide instructions on how to test the app.",
+  },
+  {
+    type: "input",
+    name: "username",
+    message: "Please enter you GitHub username;",
+    validate: (githubInput) => {
+      if (githubInput) {
+        return true;
+      } else {
+        console.log("Please enter you GitHub username!");
+        return false;
+      }
+    },
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "Please enter your email; ",
+    validate: (emailInput) => {
+      if (emailInput) {
+        return true;
+      } else {
+        console.log("Please enter your email!");
+        return false;
+      }
+    },
+  },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
